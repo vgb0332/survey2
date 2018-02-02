56ba25449cbe6dac4114edfe89d86845f583ada3
gunheekim90 <gunhee21@gmail.com>
2018-02-03 00:26:50 +0900

    data api

--- router/client/dataRoutes.js
+++ router/client/dataRoutes.js
@@ -6,8 +6,59 @@ var cert = "asldjkhf12409uaslkjllkn23lrn9f";
 
 module.exports = function(app,logger){
 
-    app.post("/client/test",middleware.authMiddleWare(),(req,res)=>{
-        res.send("success");
+	/*
+		
+		data structure
+
+		tag
+		title
+		content
+		category
+		date
+
+
+	*/
+
+    app.get("/client/getData/all",(req,res)=>{
+    	db.datas.findAll().then((result)=>{
+    		var resultArray = [];
+    		for(var rs in result){
+    			if(result.hasOwnProperty(rs)){
+    				resultArray.push(result[rs].dataValues);
+    			}
+    		}
+    		var node = {
+                success : true,
+                data : resultArray
+            }
+            res.json(node)
+    	})
     })
+
+    app.get("/client/getData/five",(req,res)=>{
+    	
+    })
+
+    app.push("client/pushContent",middleware.authMiddleWare(),(req,res)=>{
+
+    	console.log(req.body);
+    	db.datas.create(req.body).then((err,result)=>{
+    		var node = {
+                success : true,
+                message : "success",
+                callbackData : req.body
+            }
+            res.json(node)
+    	}).catch((err)=>{
+    		console.log(err);
+    		var node = {
+                success : false,
+                message : "data create fail"
+            }
+            res.json(node)
+    	})
+    })
+
+
 }
 
