5e9e0cfac3dc290fb2b870122b30981422bd99d2
gunheekim90 <gunhee21@gmail.com>
2018-06-08 21:37:28 +0900

    a

--- server.js
+++ server.js
@@ -50,10 +50,10 @@ app.use(cors(corsOptions))
 app.use(function(req, res, next) {
   var allowedOrigins = ['http://52.79.128.87', 'http://localhost:3000', 'http://127.0.0.1:80', 'http://localhost:80','http://127.0.0.1:8020','http://localhost:8020','http://localhost','http://127.0.0.1'];
   var origin = req.headers.origin;
-  if(allowedOrigins.indexOf(origin) > -1){
-       res.setHeader('Access-Control-Allow-Origin', origin);
-  }
-  // res.header('Access-Control-Allow-Origin', '*');
+  // if(allowedOrigins.indexOf(origin) > -1){
+  //      res.setHeader('Access-Control-Allow-Origin', origin);
+  // }
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.header('Access-Control-Allow-Credentials', true);
