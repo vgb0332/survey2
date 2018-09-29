eb478da4caab26b729dfc1c03b4041c4e746f5d4
gunheekim90 <gunhee21@gmail.com>
2018-09-29 21:55:44 +0900

    a

--- public/client/js/calendarInit.js
+++ public/client/js/calendarInit.js
@@ -23,8 +23,8 @@ $(document).ready(function() {
                       let tempObject = {
                         "id":eventDatas[i].id,
                         "title":eventDatas[i].title,
-                        "start":eventDatas[i].startTimeFormat,
-                        "end":eventDatas[i].endTimeFormat,
+                        "start":new Date(eventDatas[i].startTimeFormat),
+                        "end":new Date(eventDatas[i].endTimeFormat),
                         "content" : {
                           "location":eventDatas[i].location,
                           "anger" : eventDatas[i].anger,
