f83442bfe5ba3d23f0b58c4b9ba89bdd28969d41
Yongsuk Mark Jung <vgb000123@gmail.com>
2019-09-11 20:30:46 +0900

    a

--- public/client/js/calendarInit.js
+++ public/client/js/calendarInit.js
@@ -10,8 +10,8 @@ $(document).ready(function() {
   var testMonth = 9;
   var testStartDate = 29;
   var testEndDate = 30;
-  var startTestDate = '2018-10-24';
-  var endTestDate = '2018-10-25';
+  var startTestDate = '2019-09-18';
+  var endTestDate = '2018-09-19';
   var testDate = new moment('2018-10-25');
   var testNextDate = new moment('2018-10-25');
   var startHour = 7;
